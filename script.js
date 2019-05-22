9e215283698473293f4260185f1831ad4818b6d1
dvtung1 <dvtung98@gmail.com>
2019-05-22 11:16:28 +0700

    remove button to hinge, now auto hinge when the typed lib is completed

--- script.js
+++ script.js
@@ -10,21 +10,29 @@ var typed = new Typed("#typed", {
 	strings: [
 		"Hello, World!",
 		"My name is Tung",
-		"Welcome to my website <i style='font-size: 30px' class='em em-100'></i>"
+		"Let's begin <i style='font-size: 30px' class='em em-100'></i>"
 	],
 	typeSpeed: 60,
 	showCursor: true,
 	startDelay: 700,
 	backDelay: 950,
 	backSpeed: 20,
-	onComplete: self => {}
+	onComplete: self => {
+		setTimeout(() => {
+			const element = document.querySelector(".jumbotron");
+			element.classList.add("animated", "hinge", "slower");
+			element.addEventListener("animationend", function() {
+				element.style.display = "none";
+			});
+		}, 1500);
+	}
 });
 
-const buttonHinge = document.querySelector("#hinge");
-buttonHinge.addEventListener("click", () => {
-	const element = document.querySelector(".jumbotron");
-	element.classList.add("animated", "hinge", "slower");
-	element.addEventListener("animationend", function() {
-		element.style.display = "none";
-	});
-});
+// const buttonHinge = document.querySelector("#hinge");
+// buttonHinge.addEventListener("click", () => {
+// 	const element = document.querySelector(".jumbotron");
+// 	element.classList.add("animated", "hinge", "slower");
+// 	element.addEventListener("animationend", function() {
+// 		element.style.display = "none";
+// 	});
+// });
