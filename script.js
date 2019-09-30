893231f8f72b77fbd40b23d55be8e9737d408e85
dvtung1 <dvtung98@gmail.com>
2019-09-30 19:19:57 -0400

    change resume structure to timeline

--- script.js
+++ script.js
@@ -1,6 +1,53 @@
 // // Can also be included with a regular script tag
 // import Typed from "typed.js";
 
+$(function() {
+	window.sr = ScrollReveal();
+
+	if ($(window).width() < 768) {
+		if ($(".timeline-content").hasClass("js--fadeInLeft")) {
+			$(".timeline-content")
+				.removeClass("js--fadeInLeft")
+				.addClass("js--fadeInRight");
+		}
+
+		sr.reveal(".js--fadeInRight", {
+			origin: "right",
+			distance: "300px",
+			easing: "ease-in-out",
+			duration: 800
+		});
+	} else {
+		sr.reveal(".js--fadeInLeft", {
+			origin: "left",
+			distance: "300px",
+			easing: "ease-in-out",
+			duration: 800
+		});
+
+		sr.reveal(".js--fadeInRight", {
+			origin: "right",
+			distance: "300px",
+			easing: "ease-in-out",
+			duration: 800
+		});
+	}
+
+	sr.reveal(".js--fadeInLeft", {
+		origin: "left",
+		distance: "300px",
+		easing: "ease-in-out",
+		duration: 800
+	});
+
+	sr.reveal(".js--fadeInRight", {
+		origin: "right",
+		distance: "300px",
+		easing: "ease-in-out",
+		duration: 800
+	});
+});
+
 var options = {
 	strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
 	typeSpeed: 40
